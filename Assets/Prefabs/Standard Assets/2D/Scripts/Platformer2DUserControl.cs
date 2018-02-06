95b08e4a69aca27b856525347c430969417b56b5
Alain Maubert <maubert_alain@hotmail.com>
2018-02-06 15:10:06 -0600

    adding SimpleTouchInput
    ...

--- Assets/Prefabs/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
+++ Assets/Prefabs/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -24,13 +24,27 @@ namespace UnityStandardAssets._2D
                 // Read the jump input in Update so button presses aren't missed.
                 m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
             }
+
+			// Counts the number of fingers of the user that are touching the screen
+			int fingerCount = 0;
+			foreach (Touch touch in Input.touches) {
+				if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+					fingerCount++;
+			}
+			if (fingerCount > 0)
+				print("User has " + fingerCount + " finger(s) touching the screen");
         }
 
 
         private void FixedUpdate()
         {
-            // Read the inputs.
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            // Read keyboard keys as inputs
+            //float h = CrossPlatformInputManager.GetAxis("Horizontal");
+
+			// For using acceleration in x axis as an input to move.
+			Vector3 acceleration = Input.acceleration;
+			float h = acceleration.x;
+
             // Pass all parameters to the character control script.
             m_Character.Move(h, m_Jump);
             m_Jump = false;
